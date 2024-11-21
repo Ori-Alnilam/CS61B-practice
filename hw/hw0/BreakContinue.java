2e9a92ab193f7f7b7c74ba4560e402504880fc0d
Ori-Alnilam <nachocheese@163.com>
2024-11-21 14:37:00 +0800

    Finish hw0

---
+++ hw/hw0/BreakContinue.java
@@ -0,0 +1,26 @@
+public class BreakContinue {
+    public static void windowPosSum(int[] a, int n) {
+        for (int i = 0; i < a.length; i++) {
+            // Skip negative values
+            if (a[i] < 0) {
+                continue;
+            }
+            for (int j = 1; j <= n; j++) {
+                // Avoid going over the end of the array
+                if (i + j > a.length - 1) {
+                    break;
+                }
+                a[i] += a[i + j]; 
+            }
+        }
+    }
+  
+    public static void main(String[] args) {
+      int[] a = {1, 2, -3, 4, 5, 4};
+      int n = 3;
+      windowPosSum(a, n);
+  
+      // Should print 4, 8, -3, 13, 9, 4
+      System.out.println(java.util.Arrays.toString(a));
+    }
+  }

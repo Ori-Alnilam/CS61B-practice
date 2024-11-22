6582e948f522c90a39bfd7cf3b8859549f4381b4
Ori-Alnilam <nachocheese@163.com>
2024-11-22 16:13:05 +0800

    Finish intro2

---
+++ intro2/DogLauncher.java
@@ -0,0 +1,9 @@
+public class DogLauncher {
+    public static void main(String[] args) {
+        Dog d1 = new Dog(15);
+        Dog d2 = new Dog(100);
+
+        Dog bigger = Dog.maxDog(d1, d2);
+        bigger.makeNoise();
+    }
+}

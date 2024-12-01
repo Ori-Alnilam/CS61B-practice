99de8fcd0877bc5844ca4a95e82fe1d95c1976a6
Ori-Alnilam <nachocheese@163.com>
2024-12-01 16:50:59 +0800

    Add lists2

---
+++ src/lists2/IntList.java
@@ -0,0 +1,45 @@
+package lists2;
+
+public class IntList {
+    public int first;
+    public IntList rest;
+
+    public IntList(int f, IntList r) {
+        first = f;
+        rest = r;
+    }
+
+    public int size() {
+        if (rest == null) {
+            return 1;
+        }
+        return 1 + this.rest.size();
+    }
+
+    public int iterativeSize() {
+        IntList p = this;
+        int size = 0;
+        while (p != null) {
+            size++;
+            p = p.rest;
+        }
+        return size;
+    }
+
+    public int get(int i) {
+        if (i == 0) {
+            return first;
+        }
+        return rest.get(i - 1);
+    }
+
+    public static void main(String[] args) {
+        IntList L = new IntList(15, null);
+        L = new IntList(10, L);
+        L = new IntList(5, L);
+
+        System.out.println(L.size());
+        System.out.println(L.iterativeSize());
+        System.out.println(L.get(1));
+    }
+}
---
+++ src/lists2/SLList.java
@@ -0,0 +1,34 @@
+package lists2;
+
+public class SLList {
+    private static class IntNode {
+        public int item;
+        public IntNode next;
+
+        public IntNode(int i, IntNode n) {
+            item = i;
+            next = n;
+        }
+    }
+    private IntNode first;
+
+    public SLList(int x) {
+        first = new IntNode(x, null);
+    }
+
+    public void addFirst(int x) {
+        first = new IntNode(x, first);
+    }
+
+    public int getFirst() {
+        return first.item;
+    }
+
+    public static void main(String[] args) {
+        SLList L = new SLList(15);
+        L.addFirst(10);
+        L.addFirst(5);
+        System.out.println(L.getFirst());
+
+    }
+}

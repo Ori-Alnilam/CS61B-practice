44d8d8e63f5c3d16e4fc2dec0aff1af3229f0f82
Ori-Alnilam <nachocheese@163.com>
2024-12-03 23:46:07 +0800

    Using generics

--- src/lists3/SLList.java
+++ src/lists3/SLList.java
@@ -1,54 +1,54 @@
 package lists3;
 
-public class SLList {
+/** Using generics to support creating linked lists of different types. */
+public class SLList<T> {
     /** 嵌套类 */
-    public static class IntNode {
-        public int item;
-        public IntNode next;
+    public class StuffNode { // Delete "static"
+        public T item;
+        public StuffNode next;
 
-        public IntNode(int i, IntNode n) {
+        public StuffNode(T i, StuffNode n) {
             item = i;
             next = n;
         }
-    }
-    /** The first item (if it exists) is at sentinel.next. */
-    private IntNode sentinel;
+    }    /** The first item (if it exists) is at sentinel.next. */
+    private final StuffNode sentinel;
     private int size;
 
     /** 构造函数1: 空列表 */
     public SLList() {
         // Empty List
-        sentinel = new IntNode(63, null);
+        sentinel = new StuffNode(null, null);
         size = 0;
     }
     /** 构造函数2 */
-    public SLList(int x) {
-        sentinel = new IntNode(63, null);
-        sentinel.next = new IntNode(x, null);
+    public SLList(T x) {
+        sentinel = new StuffNode(null, null);
+        sentinel.next = new StuffNode(x, null);
         size = 1;
     }
 
     /** Add x to the front of the list. */
-    public void addFirst(int x) {
+    public void addFirst(T x) {
         size++;
 
-        sentinel.next = new IntNode(x, sentinel.next);
+        sentinel.next = new StuffNode(x, sentinel.next);
     }
 
     /** Add x to the end of the list. */
-    public void addLast(int x) {
+    public void addLast(T x) {
         size++;
 
-        IntNode p = sentinel;
+        StuffNode p = sentinel;
 
         while (p.next != null) {
             p = p.next;
         }
-        p.next = new IntNode(x, null);
+        p.next = new StuffNode(x, null);
     }
 
     /** Return the first item in the list. */
-    public int getFirst() {
+    public T getFirst() {
         return sentinel.next.item;
     }
 
@@ -57,12 +57,18 @@ public class SLList {
     }
 
     public static void main(String[] args) {
-        SLList emptyList = new SLList();
+        SLList<String> L2 = new SLList<>();
+        L2.addLast("apple");
+        L2.addFirst("orange");
+        System.out.println(L2.getFirst());
+        System.out.println(L2.size());
+
+        SLList<Integer> emptyList = new SLList<>();
         emptyList.addLast(20);
         System.out.println(emptyList.getFirst());
         System.out.println(emptyList.size());
 
-        SLList L = new SLList(10);
+        SLList<Integer> L = new SLList<>(10);
         L.addFirst(5);
         L.addLast(15);
         System.out.println(L.getFirst());

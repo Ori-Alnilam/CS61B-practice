9f30d31a8aa2f1ae3ab0dd481f4939da8b9ecddb
Ori-Alnilam <nachocheese@163.com>
2024-12-04 20:40:39 +0800

    Ad Hoc Testing

--- src/testing/TestSort.java
+++ src/testing/TestSort.java
@@ -1,7 +1,20 @@
 package testing;
 
 public class TestSort {
-    public static void main(String[] args) {
+    public static void testSort() {
+        String[] input = {"i", "have", "an", "egg"};
+        String[] expected = {"an", "egg", "have", "i"};
 
+        Sort.sort(input);
+        for (int i = 0; i < input.length; i++) {
+            if (!input[i].equals(expected[i])) {
+                System.out.println("Mismatch in position " + i + ", expected: " + expected[i] + ", but got: " + input[i]);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        testSort();
     }
 }

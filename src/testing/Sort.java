25ea10117799cd6ad6773c580d060618db3f26c9
Ori-Alnilam <nachocheese@163.com>
2024-12-04 21:06:16 +0800

    Add Start Comments

--- src/testing/Sort.java
+++ src/testing/Sort.java
@@ -1,7 +1,10 @@
 package testing;
 
 public class Sort {
+    /** Sorts strings destructively. */
     public static void sort(String[] x) {
-
+        // find the smallest item
+        // move it to the front
+        // selection sort the rest (using recursion?)
     }
 }

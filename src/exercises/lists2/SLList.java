5c46ccfd8041b32ba37552eec27ab974cde8a058
Ori-Alnilam <nachocheese@163.com>
2024-12-10 21:49:57 +0800

    Finish SLList(int[] arr)

--- src/exercises/lists2/SLList.java
+++ src/exercises/lists2/SLList.java
@@ -28,6 +28,17 @@ public class SLList {
         size = 1;
     }
 
+    /** 构造函数3：接收整数数组，创建链表 */
+    public SLList(int[] arr) {
+        sentinel = new IntNode(63, null);
+        IntNode p = sentinel;
+        for (int i : arr) {
+            p.next = new IntNode(i, null);
+            p = p.next;
+        }
+        size = arr.length;
+    }
+
     /** Add x to the front of the list. */
     public void addFirst(int x) {
         size++;
@@ -67,18 +78,34 @@ public class SLList {
         return size;
     }
 
+    public void printList() {
+        IntNode p = sentinel;
+        while (p.next != null) {
+            System.out.print(p.next.item + " -> ");
+            p = p.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String[] args) {
+        // 测试空链表
         SLList emptyList = new SLList();
         emptyList.deleteFirst();
         emptyList.addLast(20);
         System.out.println(emptyList.getFirst());
         System.out.println(emptyList.size());
 
+        // 测试非空链表
         SLList L = new SLList(10);
         L.addFirst(5);
         L.addLast(15);
         L.deleteFirst();
         System.out.println(L.getFirst());
         System.out.println(L.size());
+
+        // 测试整数数组创建链表
+        SLList L2 = new SLList(new int[]{1, 2, 3});
+        L2.printList();
+        System.out.println(L2.size());
     }
 }
